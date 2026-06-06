5ad0a6fb86d34145beb53dc873da377db54ced46
Muhammad Esmael <92602172+Hamoi1@users.noreply.github.com>
2026-06-06 20:17:07 +0300

    Add Kurdish (ckb) language messages for captcha

---
+++ resources/lang/ckb/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'actions' => [
+        'refresh' => 'نوێکردنەوەی کاپچا',
+    ],
+    'alt' => 'وێنەی کاپچا',
+    'direction' => 'rtl',
+    'validation' => [
+        'failed' => 'سەلماندنی کاپچا سەرکەوتوو نەبوو.',
+    ],
+];
